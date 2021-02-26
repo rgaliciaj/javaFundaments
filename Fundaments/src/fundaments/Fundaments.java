d884ea93bfd7a84f3e9d779bc344a26a8e3e59a8
rgaliciaj <rgaliciaj@miumg.edu.gt>
2021-02-26 16:29:43 -0600

    anything

--- Fundaments/src/fundaments/Fundaments.java
+++ Fundaments/src/fundaments/Fundaments.java
@@ -5,6 +5,8 @@
  */
 package fundaments;
 
+import java.util.Scanner;
+
 
 
 /**
@@ -17,12 +19,11 @@ public class Fundaments {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String resultado = (1 > 2) ? "verdadero" : "false";
-        System.out.println("resultado = " + resultado);
-        
-        int numero = 8;
-        resultado = (numero % 2 == 0) ? "numero par" : "numero impar";
-        System.out.println("resultado = " + resultado);
+         boolean condicion = true;
+         
+         if(condicion){
+             
+         }
     }
     
 }
